9f5b4523dae0168c98540d1cdc9c4fd3bb0da08a
STIRLICO <141484109+STIRLICO@users.noreply.github.com>
2025-07-08 13:33:50 +0300

    Update Node.java

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,27 +1,45 @@
 import jdk.internal.org.objectweb.asm.tree.InsnList;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Node {
+    String id;
+    String name;
+    List<Node> childrens = new ArrayList<Node>();
+
     public Node(String name) {
+        this.id = "template";
+        this.name = name;
     }
 
     public void addChildren(Node child) {
+        this.childrens.add(child);
     }
 
     public List<Node> getChildrens() {
-
-        return null;
+        return this.childrens;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public boolean findChildren(String name) {
-        return true;
+        List<Node> temp = this.childrens;
+        for (Node node : temp) {
+            if (Objects.equals(name, node.getName())){return true;}
+        }
+        return false;
     }
 
     public void deleteChildren(String name) {
+    }
+
+    public void deleteAllChildrens() {
+    }
+
+    public void editNode(String notRoot) {
     }
 }
