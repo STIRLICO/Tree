01a998837f40b08100b02b5b2b1db7ec6a7f7fe3
STIRLICO <141484109+STIRLICO@users.noreply.github.com>
2025-07-08 13:05:18 +0300

    Create Node.java

---
+++ src/main/java/Node.java
@@ -0,0 +1,27 @@
+import jdk.internal.org.objectweb.asm.tree.InsnList;
+
+import java.util.List;
+
+public class Node {
+    public Node(String name) {
+    }
+
+    public void addChildren(Node child) {
+    }
+
+    public List<Node> getChildrens() {
+
+        return null;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public boolean findChildren(String name) {
+        return true;
+    }
+
+    public void deleteChildren(String name) {
+    }
+}
